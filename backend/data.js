184dd323fc34d9f5310d220afeb4a3cba9d4a73f
Eduard <ivan_eduard13@yahoo.com>
2021-11-11 22:09:29 +0200

    start with mongodb.

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Eduard',
+            email: 'admin@exampled.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'Mihai',
+            email: 'user@exampled.com',
+            password: bcrypt.hashSync('4321', 8),
+            isAdmin: true,
+        },
+    ],
     products:[
         {
             _id:'1',
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -62,7 +62,7 @@ export default function CartScreen(props) {
                                                 ))}
                                             </select>
                                         </div>
-                                        <div>
+                                        <div className="min-5">
                                             ${item.price}
                                         </div>
                                         <div>
