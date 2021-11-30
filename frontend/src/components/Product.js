01cfd6773b48aaa0099a1c9bb3fea25c33436ae6
Eduard <ivan_eduard13@yahoo.com>
2021-11-30 18:40:21 +0200

    no message

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -20,7 +20,7 @@ export default function Product(props) {
           <div className="row">
             <div className="price">${product.price}</div>
             <div>
-              <Link to={`/seller/${product.seller._id}`}>
+              <Link to={`/seller/${product.seller.seller._id}`}>
                 {product.seller.name}
               </Link>
             </div>
